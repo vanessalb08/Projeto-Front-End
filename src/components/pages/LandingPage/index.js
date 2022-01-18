4029093acb119b4a9b893fb0dbb5551e6044a8c1
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2022-01-18 10:57:10 -0300

    Comitão

---
+++ src/components/pages/LandingPage/index.js
@@ -0,0 +1,68 @@
+import react , { Component } from "react";
+import {Form, FormGroup, Label, Input, Button, Alert} from 'reactstrap'; //informa entre {} o que quer importar
+import Header from "../../Header";
+
+export default class LeadCollector extends Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            message : this.props.state?this.props.state.message: '',
+        };
+    } 
+
+    saveLead = () => {
+        const url = "http://localhost:8080/leads";
+        const data = {
+            nome: this.nome,
+            email: this.email,
+            observacoes: this.observacoes,
+        };
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+        };
+        fetch(url, requestInfo)
+        .then(response => {
+            if(!response.ok){
+              throw new Error("Algo de errado não está certo")
+            }
+                console.log("Salvo com sucesso!")
+        })
+      }
+    render(){
+        return(
+            <div className="Collector">
+                <Header titulo="Mais informações? Cadastre-se"/>
+                <hr/>
+        
+                <Form>
+                    <FormGroup>
+                        <Label for="nome"> Nome: </Label>
+                        <Input type="text" id="nome" onChange={e => this.nome = e.target.value} placeholder="Seu nome" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email"> Email: </Label>
+                        <Input type="text" id="email" onChange={e => this.email = e.target.value} placeholder="Seu email" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="observacoes"> Observações: </Label>
+                        <Input
+                        type="text"
+                        id="observacoes"
+                        onChange={e => this.observacoes = e.target.value}
+                        placeholder="Alguma observação que queira nos contar?"
+                        />
+                    </FormGroup>
+                    <Button color="dark" block onClick={this.saveLead}>
+                        {" "}
+                        ENVIAR
+                    </Button>
+                    </Form>
+                </div>
+        );
+    }
+}
---
+++ src/components/pages/Login/index.js
@@ -0,0 +1,70 @@
+import react , { Component } from "react";
+import {Form, FormGroup, Label, Input, Button, Alert} from 'reactstrap'; //informa entre {} o que quer importar
+import Header from "../../Header";
+
+export default class Login extends Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            message : this.props.state?this.props.state.message: '',
+        };
+    }   
+
+    signIn = () => {
+        const url = "http://localhost:8080/login";
+        const data = {
+            email: this.email,
+            senha: this.password,
+        };
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+        };
+        fetch(url, requestInfo)
+            .then(response => {
+                if(response.ok){
+                    console.log("O login foi realizado com sucesso.")
+                    return response.headers.get("Authorization")
+                }
+                throw new Error("Login inválido")
+            }).then(token => {
+                localStorage.setItem('token', token);
+            }).catch( e => {
+                this.setState({message: e.message})
+                console.log(this.email, this.password)
+            });
+    }
+
+
+    render(){
+        return(
+            //Componentes são com letra maiúscula
+            <div className="CaixaLogin">
+                <Header titulo="Login"/>
+                <hr/>
+                {
+                    this.state.message !== '' ? (
+                        <Alert color = 'danger' className='text-center'>{this.state.message}</Alert>
+                    ) : ''
+                }
+            <Form>
+                <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input type="text" id="email" onChange={e => this.email = e.target.value} placeholder="Informe o seu email" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for ="password">Senha</Label>
+                    <Input type="password" id="password" onChange={e => this.password = e.target.value} placeholder="Informe a sua senha"/>
+                </FormGroup>
+                <Button color="primary" onClick={this.signIn}>Entrar</Button>
+            </Form>
+
+           
+            </div>
+        );
+    }
+}
