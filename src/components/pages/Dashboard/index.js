7281aef76313afd3c7f569a25015a8caff374732
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2022-01-19 12:02:51 -0300

    feat: renderizar leads recebidos da api

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,78 +1,123 @@
-import react, {Component} from "react";
-import {Button, Form, FormGroup,Label, Input, Alert, Table} from 'reactstrap';
+import react, { Component } from "react";
+import { Button, Form, FormGroup, Label, Input, Alert, Table } from 'reactstrap';
 
 class ListLead extends Component {
-    render() {
-      return (
-        <Table className="table-bordered text-center">
-          <thead className="thead-dark">
-            <tr>
-              <th>Nome</th>
-              <th>Email</th>
-              <th>Obs.</th>
-              <th>Actions asdf</th>
+  render() {
+    const { leads } = this.props;
+    return (
+      <Table className="table-bordered text-center">
+        <thead className="thead-dark">
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Obs.</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leads.map((lead) => (
+            <tr key={lead.email}>
+              <td>{lead.nome}</td>
+              <td>{lead.email}</td>
+              <td>{lead.observacoes}</td>
+              <td>
+                <Button 
+                  color="info"
+                  size="sm"
+                >
+                  Editar
+                </Button>
+                <Button
+                color="danger"
+                size="sm"
+                >
+                  Deletar
+                </Button>
+              </td>
             </tr>
-          </thead>
-        </Table>
-      );
-    }
+          ))}
+        </tbody>
+      </Table>
+    );
   }
-  
-  class FormLead extends Component {
-    render() {
-      return (
-        <Form>
-          <FormGroup>
-            <div className="form-row">
-              <Label for="nome"> Nome </Label>
-              <Input id="nome" type="text" placeholder="Informe o nome do lead" />
-            </div>
-          </FormGroup>
-          <FormGroup>
-            <div className="form-row">
-              <Label for="email"> Email </Label>
-              <Input
-                id="email"
-                type="text"
-                placeholder="Informe o email do lead"
-              />
-            </div>
-          </FormGroup>
-          <FormGroup>
-            <div className="form-row">
-              <Label for="observations"> Observações </Label>
-              <Input
-                id="observations"
-                type="text"
-                placeholder="Observações do lead"
-              />
-            </div>
-          </FormGroup>
-          <Button color="primary" block>
-            ATUALIZAR
-          </Button>
-        </Form>
-      );
-    }
+}
+
+class FormLead extends Component {
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="nome"> Nome </Label>
+            <Input id="nome" type="text" placeholder="Informe o nome do lead" />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="email"> Email </Label>
+            <Input
+              id="email"
+              type="text"
+              placeholder="Informe o email do lead"
+            />
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <div className="form-row">
+            <Label for="observations"> Observações </Label>
+            <Input
+              id="observations"
+              type="text"
+              placeholder="Observações do lead"
+            />
+          </div>
+        </FormGroup>
+        <Button color="primary" block>
+          ATUALIZAR
+        </Button>
+      </Form>
+    );
   }
-  
-  class Dashboard extends Component {
-    render() {
-      return (
-        <div>
-          <div className="row">
-            <div className="col-md-6 my-3">
-              <h2 className="font-weight-bold text-center">ATUALIZAR LEAD</h2>
-              <FormLead />
-            </div>
-            <div className="col-md-6 my-3">
-              <h2 className="font-weight-bold text-center">LISTA DE LEADS </h2>
-              <ListLead />
-            </div>
+}
+
+class Dashboard extends Component {
+  url = "http://localhost:8080/leads";
+
+  state = {
+    leads: [],
+  };
+
+  componentDidMount() {
+    let token = localStorage.getItem("token");
+    const requestInfo = {
+      method: "GET",
+      headers: new Headers({
+        "Content-type": "application/json",
+        Authorization: token,
+      }),
+    };
+    fetch(this.url, requestInfo)
+      .then((response) => response.json())
+      .then((leads) => this.setState({ leads }))
+      .catch((e) => console.log(e));
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-6 my-3">
+            <h2 className="font-weight-bold text-center">ATUALIZAR LEAD</h2>
+            <FormLead />
+          </div>
+          <div className="col-md-6 my-3">
+            <h2 className="font-weight-bold text-center">LISTA DE LEADS </h2>
+            <ListLead leads={this.state.leads}/>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default Dashboard;
+}
+
+export default Dashboard;
